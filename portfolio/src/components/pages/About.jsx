73540a1b4964043a8012bcb3ba5a9e3b388d1739
omri310 <omrifrazer@gmail.com>
2024-04-12 12:06:33 -0700

    added info on pages file

--- portfolio/src/components/pages/About.jsx
+++ portfolio/src/components/pages/About.jsx
@@ -3,9 +3,9 @@ export default function About() {
     <div>
       <h1>About Page</h1>
       <p>
-      <div class="alert alert-warning" role="alert">
-Professional skills...
-</div>
+        <div class="alert alert-warning" role="alert">
+          With a background in Business Analytics and Project Management, I am pursing a certificate in Full-Stack Web Development from UCLA Extension. I have learned to write, build responsive web applications using JavaScript, React.js, Node.js, Python, MySQL and in creating bug-free systems to meet client expectations. I am dedicated and passionate to develop, integrate multi-faceted applications to facilitate smooth business operations.
+        </div>
       </p>
     </div>
   );
--- portfolio/src/components/pages/Blog.jsx
+++ portfolio/src/components/pages/Blog.jsx
@@ -18,68 +18,68 @@ export default function Blog() {
           </div>
         </div>
         <div className="card">
-          <img src="..." className="card-img-top" alt="..." />
+          <img src="/screenshots/weatherpredictor.png" className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">Weather Dashboard</h5>
-            <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            <p className="card-text">An assignment to show the weather in different cities</p>
             <p className="card-text"><small className="text-body-secondary">
-              <a href="https://omri310.github.io/marketing_website/" target="_blank">Live</a>
+              <a href="https://omri310.github.io/5days_weather_forecast/" target="_blank">Live</a>
             </small></p>
             <p className="card-text"><small className="text-body-secondary">
-              <a href="https://github.com/omri310/marketing_website" target="_blank">Github</a>
+              <a href="https://github.com/omri310/5days_weather_forecast" target="_blank">Github</a>
             </small></p>
           </div>
         </div>
         <div className="card">
-          <img src="..." className="card-img-top" alt="..." />
+          <img src="/screenshots/passwordgenerator.png" className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <h5 className="card-title">Password Generator</h5>
+            <p className="card-text">An assignment to show how passwords are generated.</p>
             <p className="card-text"><small className="text-body-secondary">
-              <a href="https://omri310.github.io/marketing_website/" target="_blank">Live</a>
+              <a href="https://omri310.github.io/password_generator/" target="_blank">Live</a>
             </small></p>
             <p className="card-text"><small className="text-body-secondary">
-              <a href="https://github.com/omri310/marketing_website" target="_blank">Github</a>
+              <a href="https://github.com/omri310/password_generator" target="_blank">Github</a>
             </small></p>
           </div>
         </div>
       </div>
       <div className="card">
-        <img src="/screenshots/coderefactor.png" className="card-img-top img-fluid" alt="..." />
+        <img src="/screenshots/workdayscheduler.png" className="card-img-top img-fluid" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Code refactor</h5>
-          <p className="card-text">An assignment to show importance of semantic tags</p>
+          <h5 className="card-title">Day Work Scheduler</h5>
+          <p className="card-text">An assignment to show importance of scheduling at work</p>
           <p className="card-text"><small className="text-body-secondary">
-            <a href="https://omri310.github.io/marketing_website/" target="_blank">Live</a>
+            <a href="https://omri310.github.io/day_work_scheduler/" target="_blank">Live</a>
           </small></p>
           <p className="card-text"><small className="text-body-secondary">
-            <a href="https://github.com/omri310/marketing_website" target="_blank">Github</a>
+            <a href="https://github.com/omri310/day_work_scheduler" target="_blank">Github</a>
           </small></p>
         </div>
       </div>
       <div className="card">
-        <img src="/screenshots/coderefactor.png" className="card-img-top img-fluid" alt="..." />
+        <img src="/screenshots/employeetracker.png" className="card-img-top img-fluid" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Code refactor</h5>
-          <p className="card-text">An assignment to show importance of semantic tags</p>
+          <h5 className="card-title">Employee Tracker</h5>
+          <p className="card-text">An assignment to view and manage the departments, roles, and employees in my company so that I can organize and plan my business</p>
           <p className="card-text"><small className="text-body-secondary">
-            <a href="https://omri310.github.io/marketing_website/" target="_blank">Live</a>
+            <a href="" target="_blank">Live</a>
           </small></p>
           <p className="card-text"><small className="text-body-secondary">
-            <a href="https://github.com/omri310/marketing_website" target="_blank">Github</a>
+            <a href="https://github.com/omri310/unique_employee_tracker" target="_blank">Github</a>
           </small></p>
         </div>
       </div>
       <div className="card">
-        <img src="/screenshots/coderefactor.png" className="card-img-top img-fluid" alt="..." />
+        <img src="/screenshots/techblog.png" className="card-img-top img-fluid" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Code refactor</h5>
-          <p className="card-text">An assignment to show importance of semantic tags</p>
+          <h5 className="card-title">Tech Blog</h5>
+          <p className="card-text">An assignment to CMS-style blog site so that I can publish articles, blog posts, and my thoughts and opinions</p>
           <p className="card-text"><small className="text-body-secondary">
-            <a href="https://omri310.github.io/marketing_website/" target="_blank">Live</a>
+            <a href="" target="_blank">Live</a>
           </small></p>
           <p className="card-text"><small className="text-body-secondary">
-            <a href="https://github.com/omri310/marketing_website" target="_blank">Github</a>
+            <a href="https://github.com/omri310/unique_MVC_techblog" target="_blank">Github</a>
           </small></p>
         </div>
       </div>
